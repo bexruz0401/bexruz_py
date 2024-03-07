0e9883f02954d50bf7007fb63ddaba925a31bcc4
Bexruz <bexruzzabixullayev@gmail.com>
2024-03-07 16:32:04 +0500

    python

---
+++ Order_Product-master/Product/views.py
@@ -0,0 +1,9 @@
+from rest_framework import viewsets, permissions
+from .serializers import ProductSerializer
+from .models import Product
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [permissions.AllowAny,]
